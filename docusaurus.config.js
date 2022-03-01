0b2cbfa44865854d91a8a76eb637ac40bed64665
Erik Andreazi <andreazi.erik@gmail.com>
2022-03-01 13:43:03 -0300

    Alterando repositorio github

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -26,7 +26,7 @@ const config = {
           routeBasePath: '/',
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/andreazierik/andreazierik.github.io/blob/main/',
         },
         blog: false,
         //blog: {
